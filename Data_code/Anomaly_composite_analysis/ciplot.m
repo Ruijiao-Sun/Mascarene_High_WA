58d07c603a7e181948d3b5e935d86c96afa845e3
Ruijiao Sun <29690919+Ruijiao-Sun@users.noreply.github.com>
2026-05-12 10:43:38 -0700

    Update ciplot.m

--- Data_code/Anomaly_composite_analysis/ciplot.m
+++ Data_code/Anomaly_composite_analysis/ciplot.m
@@ -1,23 +1,4 @@
 function plot_handle = ciplot(lower,upper,x,colour,alpha);
-     
-% ciplot(lower,upper)       
-% ciplot(lower,upper,x)
-% ciplot(lower,upper,x,colour)
-% ciplot(lower,upper,x,colour,alpha)
-%
-% Plots a shaded region on a graph between specified lower and upper confidence intervals (L and U).
-% l and u must be vectors of the same length.
-% Uses the 'fill' function, not 'area'. Therefore multiple shaded plots
-% can be overlayed without a problem. Make them transparent for total visibility.
-% x data can be specified, otherwise plots against index values.
-% colour can be specified (eg 'k'). Defaults to blue.
-% Raymond Reynolds 24/11/06
-%
-% Add: 5th parameter, alpha (Default=50%)
-% Pham Thai Binh 12/06/2017
-if length(lower)~=length(upper)
-    error('lower and upper vectors must be same length')
-end
 if nargin<5
     alpha=0.5;
 end
